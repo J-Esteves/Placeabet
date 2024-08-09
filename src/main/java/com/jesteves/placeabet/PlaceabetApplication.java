9266392324441563e74e0402ff4451b07f9e6d61
Esteves <joao-pedro.esteves@capgemini.com>
2024-08-09 23:50:19 +0100

    Commit inicial

---
+++ src/main/java/com/jesteves/placeabet/PlaceabetApplication.java
@@ -0,0 +1,14 @@
+package com.jesteves.placeabet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceabetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceabetApplication.class, args);
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/test/java/com/jesteves/placeabet/PlaceabetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesteves.placeabet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceabetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
